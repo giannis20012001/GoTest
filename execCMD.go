a3452c3fced8292fbd22c7a7277f516fbfa6550c
John Tsantilis <i.tsantilis@yahoo.com>
2017-10-12 16:31:22 +0300

    code refactor

---
+++ execCMD.go
@@ -0,0 +1,55 @@
+package main
+
+/**
+ * Created by John Tsantilis
+ * (i [dot] tsantilis [at] yahoo [dot] com A.K.A lumi) on 12/10/2017.
+ */
+
+
+import (
+	"os"
+	"fmt"
+	"strings"
+	"os/exec"
+	"syscall"
+
+)
+
+func main() {
+	cmd := exec.Command("ls", "/imaginary/directory")
+	var waitStatus syscall.WaitStatus
+	if err := cmd.Run(); err != nil {
+		printError(err)
+		// Did the command fail because of an unsuccessful exit code
+		if exitError, ok := err.(*exec.ExitError); ok {
+			waitStatus = exitError.Sys().(syscall.WaitStatus)
+			printOutput([]byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
+		}
+	} else {
+		// Command was successful
+		waitStatus = cmd.ProcessState.Sys().(syscall.WaitStatus)
+		printOutput([]byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
+	}
+
+}
+
+func printCommand(cmd *exec.Cmd) {
+	fmt.Printf("==> Executing: %s\n", strings.Join(cmd.Args, " "))
+
+}
+
+func printError(err error) {
+	if err != nil {
+		os.Stderr.WriteString(fmt.Sprintf("==> Error: %s\n", err.Error()))
+
+	}
+
+}
+
+func printOutput(outs []byte) {
+	if len(outs) > 0 {
+		fmt.Printf("==> Output: %s\n", string(outs))
+
+	}
+
+}
