362f1375fadf0beec6344e9817b59e370eb894c9
John Tsantilis <i.tsantilis@yahoo.com>
2017-10-25 18:24:55 +0300

    code refactor

---
+++ timeTest.go
@@ -0,0 +1,31 @@
+package main
+
+/**
+ * Created by John Tsantilis
+ * (i [dot] tsantilis [at] yahoo [dot] com A.K.A lumi) on 25/10/2017.
+ */
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+
+	switch time.Saturday {
+	case today+0:
+		fmt.Println("Today")
+	case today+1:
+		fmt.Println("Tomorrow")
+	case today+2:
+		fmt.Println("In two days")
+	default:
+		fmt.Println("Too far away")
+	}
+
+	time.Sleep(3000 * time.Millisecond)
+	fmt.Println("Just woke up")
+
+}
