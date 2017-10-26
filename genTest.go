70f9bb0618b69a644bad965131166824986a038d
John Tsantilis <i.tsantilis@yahoo.com>
2017-10-26 17:15:59 +0300

    code refactor

---
+++ genTest.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+/**
+ * Created by John Tsantilis
+ * (i [dot] tsantilis [at] yahoo [dot] com A.K.A lumi) on 26/10/2017.
+ */
+
+func main() {
+	var data []string
+	var finalData map[string][]string
+
+	temp := make(map[string][]string)
+	finalData = temp
+
+	data = append(data, "192.168.7.50")
+	data = append(data, "45045")
+
+	finalData["mysqluri"] = data
+	finalData["mysqlport"] = data
+
+	fmt.Println(finalData)
+
+}
