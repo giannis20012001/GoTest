707489081603a2fc87c4a4514d1f536ab6b2db05
John Tsantilis <i.tsantilis@yahoo.com>
2017-12-15 15:01:25 +0200

    code refactor

---
+++ mysqlTest.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+
+)
+
+/**
+ * Created by John Tsantilis (A.K.A lumi) on 14/12/2017.
+ * @author John Tsantilis <i.tsantilis [at] yahoo [dot] com>
+ */
+
+func main() {
+	fmt.Println("Go MySQL Tutorial")
+
+	// Open up our database connection.
+	db, err := sql.Open("mysql", "root:Floor53*@tcp(127.0.0.1:3306)/mysql")
+	if err != nil {
+		panic(err.Error())
+
+	}
+
+	defer db.Close()
+
+
+	// Query the square-number of 13
+	rows, err := db.Query("SHOW GLOBAL STATUS WHERE Variable_name = 'Bytes_sent'")
+	if err != nil {
+		panic(err.Error())
+
+	}
+
+	for rows.Next() {
+		var yolo string
+		var yolo2 int
+
+		err = rows.Scan(&yolo, &yolo2)
+		if err != nil {
+			panic(err.Error())
+
+		}
+
+		fmt.Printf("Yolo: %d", yolo2)
+
+	}
+
+}
---
+++ test.go
@@ -0,0 +1,17 @@
+package main
+
+/**
+ * Created by John Tsantilis (A.K.A lumi) on 15/12/2017.
+ * @author John Tsantilis <i.tsantilis [at] yahoo [dot] com>
+ */
+
+import (
+	"fmt"
+	"strings"
+
+)
+
+func main() {
+	fmt.Println(strings.ToLower("Bytes_received"))
+
+}
